38a68f29359dc803a9b239101b0f7adfbdf1bf3f
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-06-04 08:29:54 -0300

    Lançando a exceção para a data de nascimento

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -1,4 +1,54 @@
 package model;
 
+import exceptions.DataInvalidaException;
+
+import java.time.LocalDate;
+
 public class Pessoa {
+
+    private String cpf;
+    private String nome;
+    private LocalDate nascimento;
+
+    public Pessoa(String cpf, String nome, LocalDate nascimento) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.nascimento = nascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) throws DataInvalidaException {
+        if(nascimento.isAfter(LocalDate.now()))
+            throw new DataInvalidaException("A data não pode ser" +
+                    "posterior a hoje");
+        this.nascimento = nascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", nascimento=" + nascimento +
+                '}';
+    }
 }
