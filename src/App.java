3c513a40a4b5dec0733ec041f6e476d8b5822918
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-06-04 08:40:35 -0300

    Lançando e tratando a exceção

--- src/App.java
+++ src/App.java
@@ -1,15 +1,22 @@
+import exceptions.DataInvalidaException;
+import model.Pessoa;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.time.LocalDate;
 
 public class App {
 
     public static void main(String[] args) {
-        try{
-            lerArquivo("teste.txt");
-        }catch (FileNotFoundException ex){
-            System.out.println("Arquivo não encontrado");
+        Pessoa pessoa = new Pessoa("111.111.111-01",
+                "João", LocalDate.of(2000,1,1));
+        try {
+            pessoa.setNascimento(LocalDate.of(2025,6,5));
+        } catch (DataInvalidaException e) {
+            System.out.println(e.getMessage());
         }
+        System.out.println(pessoa);
     }
 
     private static void lerArquivo(String file) throws FileNotFoundException {
